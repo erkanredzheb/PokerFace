91b4a510c85794ea0c62a9eb8168c21c1b1802ee
Aivaras Tumas <aivarastumas@eduroam-2-069.epfl.ch>
2016-11-20 12:31:24 +0100

    final release updated

--- Emotion_API_call.php
+++ Emotion_API_call.php
@@ -37,43 +37,17 @@ $scores = $first_face['scores'];
 $averages = array();
 array_push($averages, $scores['fear'], $scores['sadness'], $scores['neutral']);
 
-echo "FEAR: ".$averages[0]."<br>";
-echo "SADNESS: ".$averages[1]."<br>";
-echo "NEUTRAL: ".$averages[2]."<br>";
-
-
 $diff_bluff_fear = abs($BLUFF_MEAN_FEAR - $averages[0]);
 $diff_normal_fear = abs($NORMAL_MEAN_FEAR - $averages[0]);
 $bluffing_by_fear = ($diff_bluff_fear < $diff_normal_fear) ? true : false;
-
-if ($bluffing_by_fear){
-	echo "FEAR: TRUE<br>";
-}
-else{
-	echo "FEAR: FALSE<br>";
-}
 
 $diff_bluff_sad = abs($BLUFF_MEAN_SAD - $averages[1]);
 $diff_normal_sad = abs($NORMAL_MEAN_SAD - $averages[1]);
 $bluffing_by_sad = ($diff_bluff_sad < $diff_normal_sad) ? true : false;
 
-if ($bluffing_by_sad){
-	echo "SAD: TRUE<br>";
-}
-else{
-	echo "SAD: FALSE<br>";
-}
-
 $diff_bluff_neutral = abs($BLUFF_MEAN_NEUTRAL - $averages[2]);
 $diff_normal_neutral = abs($NORMAL_MEAN_NEUTRAL - $averages[2]);
 $bluffing_by_neutral = ($diff_bluff_neutral < $diff_normal_neutral) ? true : false;
-
-if ($bluffing_by_neutral){
-	echo "NEUTRAL: TRUE<br>";
-}
-else{
-	echo "NEUTRAL: FALSE<br>";
-}
 
 if ($bluffing_by_fear && !$bluffing_by_sad && !$bluffing_by_neutral){
 	$random_2 = $diff_normal_sad;
@@ -82,8 +56,6 @@ if ($bluffing_by_fear && !$bluffing_by_sad && !$bluffing_by_neutral){
 	$random_combined = $random_2 + $random_3;
 
 	$bluffing = ($diff_bluff_fear < $random_combined) ? true : false;
-
-	echo "HERE 1<br>";
 }
 else if (!$bluffing_by_fear && $bluffing_by_sad && $bluffing_by_neutral){
 	$random_2 = $diff_bluff_sad;
@@ -92,21 +64,14 @@ else if (!$bluffing_by_fear && $bluffing_by_sad && $bluffing_by_neutral){
 	$random_combined = $random_2 + $random_3;
 
 	$bluffing = ($diff_normal_fear < $random_combined) ? false : true;
-
-	echo "HERE 2<br>";
 }
 else{
 	$bluffing = ($bluffing_by_fear && ($bluffing_by_sad || $bluffing_by_neutral)) ? true : false;
-
-	echo "HERE 3<br>";
 }
 
 if ($bluffing){
-	echo "BLUFF";
+	echo "THIS PERSON IS BLUFFING";
 }
 else{
-	echo "NORMAL";
+	echo "THIS PERSON IS NORMAL";
 }
-
-
-
